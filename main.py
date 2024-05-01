e4a9bd93c91aba5ce5dc9527a2bfd48d322f3d14
wlya0499 <139559538+wlya0499@users.noreply.github.com>
2024-05-01 19:05:41 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+#willy aditia
+#(41823010002)
+#tugas 2
+
+part = input("Pilih bagian (A/B): ").upper()
+
+# Memeriksa input yang valid dan menjalankan sesuai pilihan
+if part == 'A':
+    print("Bagian A:")
+    n = int(input("Masukkan nilai n: "))
+    for i in range(n):
+        print(i*i)
+
+
+elif part == 'B':
+    print("Bagian B:")
+    n = int(input("Masukkan nilai n: "))
+    for i in range(n):
+        if i % 2 == 1 or i == 0:
+            print(i*i)
+else:
+    print("Pilihan tidak valid.")
+
+
+
+
+#willy aditia
+#(41823010002)
+#tugas 2
